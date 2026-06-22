27d5e0097f8d791d9d245ef6440c71266749e9a4
xinyuezhao543-hub <xinyuezhao543-hub@users.noreply.github.com>
2026-06-22 10:44:33 +0000

    deploy: 66196799219b3788fa2ff10a9502ef9ef2f977cb

--- assets/use-id-C-riT-sj.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-DqHVlqfI.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
---
+++ assets/use-id-hbXMhdK2.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-DixI49AW.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
